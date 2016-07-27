ea67ddee8392bb20e85721a5378b803dfaadaa36
MonsterMMORPG <monstermmorpg@gmail.com>
2016-07-27 23:32:19 +0300

    First commit

---
+++ FeroxRev/PokemonGo.RocketAPI/ISettings.cs
@@ -0,0 +1,15 @@
+﻿using PokemonGo.RocketAPI.Enums;
+
+namespace PokemonGo.RocketAPI
+{
+    public interface ISettings
+    {
+        AuthType AuthType { get; }
+        double DefaultLatitude { get; }
+        double DefaultLongitude { get; }
+        double DefaultAltitude { get; }
+        string GoogleRefreshToken { get; set; }
+        string PtcPassword { get; }
+        string PtcUsername { get; }
+    }
+}
